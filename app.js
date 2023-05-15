3d4aed46374f6f638bd70c38b48998a9984cbe7e
Andrew <mullinsa428@gmail.com>
2023-05-15 16:46:57 -0400

    added on/off, squareroot and squared

--- app.js
+++ app.js
@@ -24,45 +24,104 @@
 
 
 const strButs = document.querySelectorAll('.strBtn');
-const numButs = document.querySelectorAll('.numberBtn');
-const opButs = document.querySelectorAll('.operatorBtn');
+// const numButs = document.querySelectorAll('.numberBtn');
+// const opButs = document.querySelectorAll('.operatorBtn');
 const equalBut = document.getElementById('equals');
 const clearBut = document.getElementById('clearBtn');
+const squareBut = document.getElementById('expon');
+const squareRootBut = document.getElementById('sqrtbtn');
 const viewScreen = document.getElementById('viewScreen');
-
+const offOn = document.getElementById('offOn');
 
 
 let currentMath = '';
 let finalAnswer = null;
+let isOn = false;
+
+//start with the buttons disabled initially//
+strButs.forEach(function(button) {button.disabled = true});
+
+
 /// EVENT LISTENERS HERE ///
+
+offOn.addEventListener('click', () => {
+    isOn = isOn ? false : true;
+    if (isOn) {
+        viewScreen.style.backgroundColor = 'lightgray';
+        strButs.forEach(function(button) {button.disabled = false});
+    } else {
+        viewScreen.style.backgroundColor = 'black';
+        strButs.forEach(function(button) {button.disabled = true});
+    }
+})
+
+
+
+// for (let i = 0; i<strButs.length; i++) {
+//     strButs[i].addEventListener('click', doMath(i));
+// }
 
 for (let i = 0; i<strButs.length; i++) {
     strButs[i].addEventListener('click', () => {
         currentMath = `${currentMath}${strButs[i].id}`;
-        console.log(strButs[i].id);
-        console.log(currentMath);
         showCurrentMath()
         return;
     })
 }
 
-equalBut.addEventListener('click', () => {
-    finalAnswer = eval(currentMath);
-    console.log(finalAnswer);
-    viewScreen.innerText = finalAnswer;
-    return;
-})
+equalBut.addEventListener('click', equalButton);
 
-clearBut.addEventListener('click', () => {
-    currentMath = '';
-    showCurrentMath()
-    return;
-})
+// equalBut.addEventListener('click', () => {
+//     finalAnswer = eval(currentMath);
+//     console.log(finalAnswer);
+//     viewScreen.innerText = finalAnswer;
+//     return;
+// })
 
-/// FUNCTIONS HERE ///
+clearBut.addEventListener('click', clearButton);
+
+// clearBut.addEventListener('click', () => {
+//     currentMath = '';
+//     showCurrentMath()
+//     return;
+// })
+
+squareBut.addEventListener('click', squareIt);
+squareRootBut.addEventListener('click', squareRoot);
+
+/// Additional Functions ///
 
 function showCurrentMath() {
     viewScreen.innerText = currentMath;
 }
 
+function doMath(button) {
+    currentMath = `${currentMath}${strButs[button].id}`;
+    showCurrentMath()
+    return;
+}
 
+function squareIt() {
+    currentMath = `${currentMath}*${currentMath}`
+    finalAnswer = eval(currentMath);
+    viewScreen.innerText = finalAnswer;
+    return;
+}
+
+function squareRoot() {
+    finalAnswer = Math.sqrt(eval(currentMath));
+    viewScreen.innerText = finalAnswer;
+    return;
+}
+
+function equalButton() {
+        finalAnswer = eval(currentMath);
+        viewScreen.innerText = finalAnswer;
+        return;
+}
+
+function clearButton() {
+    currentMath = '';
+    showCurrentMath()
+    return;
+}
